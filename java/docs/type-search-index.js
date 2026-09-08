db58cf44937a47fa9b469f51de59fe3668419089
Terry Burton <tez@terryburton.co.uk>
2026-09-08 22:29:01 +0100

    java: Regenerate documentation

--- java/docs/type-search-index.js
+++ java/docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.gs1","l":"GMN"},{"p":"org.gs1","l":"GS1Exception"}]
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.gs1","l":"GMN"},{"p":"org.gs1","l":"GS1Exception"}];updateSearchResults();
